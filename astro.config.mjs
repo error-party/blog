b4e4de201eb26c162b7411d97a41c6eb12bf7562
frostedkitsune <92984571+frostedkitsune@users.noreply.github.com>
2025-07-28 20:31:53 +0530

    fix: update base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://error-party.github.io',
-	base: 'blog',
+	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
