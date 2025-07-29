ee2b07e2b82477117f7d4168394e3523eae9a3a9
frostedkitsune <92984571+frostedkitsune@users.noreply.github.com>
2025-07-29 17:08:15 +0530

    add web feed (rss)

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+
+export async function GET(context: { site: any; }) {
+  const blog = await getCollection('blog');
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/${post.id}/`,
+    })),
+  });
+}
