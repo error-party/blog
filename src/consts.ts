ec099673d26925535e262103244d00917b32580c
frostedkitsune <92984571+frostedkitsune@users.noreply.github.com>
2025-07-29 17:08:31 +0530

    organize structure

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Error Blog';
 export const SITE_DESCRIPTION = 'Welcome to our blog!';
+
